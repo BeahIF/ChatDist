5e17b6978f571cad50d1908e9e04a8d56a73e703
BeahIF <beatrizdacosta1@Gmail.com>
2023-05-08 15:54:25 -0300

    tentativa

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+PORT = 5050
+SERVER = 'localhost'  # socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+
+
+def connect():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    return client
+
+
+def send(client, msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
+
+
+client = connect()
+send(client, "Testando")
+input()
+send(client, DISCONNECT_MESSAGE)
